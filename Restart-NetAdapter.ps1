cb698f42f7dfefa7d0f0b31ec7b3d1e318ec77a7
Tobias Sommer <51875851+DevStyx@users.noreply.github.com>
2022-11-17 09:31:28 +0100

    Add files via upload

---
+++ Restart-NetAdapter.ps1
@@ -0,0 +1,7 @@
+﻿# Speichert alle Netzwerkadapter, die zum Zeitpunkt des ausführen den Status "UP" haben, in einer Variable.
+$NetAdapters = Get-NetAdapter | Select-Object name, status | Where-Object status -eq ‘up’
+
+# Startet jeden Netzwerkadapter einmal neu.
+foreach($NetAdapter in $NetAdapters){
+    Restart-NetAdapter -Name $NetAdapter.name
+}
