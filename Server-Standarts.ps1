7c25fdbf39dd62bc7b3e59e12a88edd49e014a5e
Tobias Sommer <51875851+DevStyx@users.noreply.github.com>
2022-11-17 09:31:17 +0100

    Add files via upload

---
+++ Server-Standarts.ps1
@@ -0,0 +1,10 @@
+﻿# Erstellen des Ordner WKW2K auf der Festplatte mit dem Betriebsystem auf Root Ebene
+New-Item -Path 'C:\WKW2K' -ItemType Directory
+# Setzt den Ordner auf erster Ebene auf Versteckt
+attrib +h "C:\WKW2K"
+
+# Erstellen innerhalb dieses Ordners weitere Ordner
+New-Item -Path 'C:\WKW2K\scripts' -ItemType Directory
+New-Item -Path 'C:\WKW2K\utils' -ItemType Directory
+
+Invoke-WebRequest -Uri https://raw.githubusercontent.com/DevStyx/Wiki/master/Powershell/Restart-NetAdapter.ps1?token=GHSAT0AAAAAABZQXUGHFBDBXILDELJAZVJ2Y3V6CUA -OutFile C:\WKW2K\scripts\Restart-NetAdapter.ps1
